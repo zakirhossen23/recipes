9119594c4c54dd5d95e276594629e7786f561e61
Zakir Hossen <73053667+zakirhossen23@users.noreply.github.com>
2022-01-11 03:01:51 +0600

    fixed min

--- templates/template-amp-meta.php
+++ templates/template-amp-meta.php
@@ -1,44 +1,45 @@
 <?php
+
 /**
  * Template: Amp-meta
  *
  * @package Recipes WordPress Theme
  */
 
-$servings            = get_post_meta( $post->ID, 'custom_meta_servings', true );
-$calories_in_serving = absint( get_post_meta( $post->ID, 'custom_meta_calories_in_serving', true ) );
-$prep_time           = absint( get_post_meta( $post->ID, 'custom_meta_prep_time', true ) );
-$cook_time           = absint( get_post_meta( $post->ID, 'custom_meta_cook_time', true ) );
+$servings            = get_post_meta($post->ID, 'custom_meta_servings', true);
+$calories_in_serving = absint(get_post_meta($post->ID, 'custom_meta_calories_in_serving', true));
+$prep_time           = absint(get_post_meta($post->ID, 'custom_meta_prep_time', true));
+$cook_time           = absint(get_post_meta($post->ID, 'custom_meta_cook_time', true));
 ?>
 
-<?php if ( ! empty( $servings ) ) : ?>
-	<b><?php esc_html_e( 'Servings', 'recipes' ); ?></b>
-	<?php echo absint( $servings ); ?> &bull;
+<?php if (!empty($servings)) : ?>
+	<b><?php esc_html_e('Servings', 'recipes'); ?></b>
+	<?php echo absint($servings); ?> &bull;
 <?php endif; ?>
 
-<?php if ( ! empty( $calories_in_serving ) ) : ?>
-	<b><?php esc_html_e( 'Calories in serving', 'recipes' ); ?></b>
-	<?php echo absint( $calories_in_serving ); ?> &bull;
+<?php if (!empty($calories_in_serving)) : ?>
+	<b><?php esc_html_e('Calories in serving', 'recipes'); ?></b>
+	<?php echo absint($calories_in_serving); ?> &bull;
 <?php endif; ?>
 
-<?php if ( ! empty( $prep_time ) ) : ?>
-	<b><?php esc_html_e( 'Mise It!', 'recipes' ); ?></b>
-	<?php mytheme_minutes_to_hr_min( $prep_time ); ?> &bull;
+<?php if (!empty($prep_time)) : ?>
+	<b><?php esc_html_e('Mise It!', 'recipes'); ?></b>
+	<?php echo absint($prep_time); ?> &bull;
 <?php endif; ?>
 
-<?php if ( ! empty( $cook_time ) ) : ?>
-	<b><?php esc_html_e( 'Make It!', 'recipes' ); ?></b>
-	<?php mytheme_minutes_to_hr_min( $cook_time ); ?>
+<?php if (!empty($cook_time)) : ?>
+	<b><?php esc_html_e('Make It!', 'recipes'); ?></b>
+	<?php echo absint($cook_time); ?>
 <?php endif; ?>
 
-<?php if ( rcps_has_any_term() ) : ?>
-	<br><b><?php esc_html_e( 'Categories', 'recipes' ); ?></b>
+<?php if (rcps_has_any_term()) : ?>
+	<br><b><?php esc_html_e('Categories', 'recipes'); ?></b>
 <?php endif; ?>
 
-<?php $taxonomies = get_object_taxonomies( 'recipe', 'names' ); ?>
+<?php $taxonomies = get_object_taxonomies('recipe', 'names'); ?>
 
-<?php foreach ( $taxonomies as $taxonomy_name ) : ?>
-	<?php if ( has_term( '', $taxonomy_name ) ) : ?>
-		<?php echo get_the_term_list( $post->ID, $taxonomy_name, '', ' ', ' ' ); ?>
+<?php foreach ($taxonomies as $taxonomy_name) : ?>
+	<?php if (has_term('', $taxonomy_name)) : ?>
+		<?php echo get_the_term_list($post->ID, $taxonomy_name, '', ' ', ' '); ?>
 	<?php endif; ?>
 <?php endforeach; ?>
--- templates/template-recipe-small.php
+++ templates/template-recipe-small.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template: Recipe Small
  *
@@ -9,61 +10,62 @@
 
 <div class="rcps-widget-item">
 	<div class="rcps-widget-image">
-		<a href="<?php echo esc_url( mytheme_get_recipe_url() ); ?>" <?php echo ( mytheme_is_external_recipe() && mytheme_get_option( 'link_external_recipes_to_source' ) ? 'target="_blank" rel="noopener"' : '' ); ?>>
+		<a href="<?php echo esc_url(mytheme_get_recipe_url()); ?>" <?php echo (mytheme_is_external_recipe() && mytheme_get_option('link_external_recipes_to_source') ? 'target="_blank" rel="noopener"' : ''); ?>>
 			<?php
 			the_post_thumbnail(
-			'img-96',
-			array(
-				'class' => 'rcps-widget-img lazyload',
-				'alt'   => get_the_title(),
-			) );
+				'img-96',
+				array(
+					'class' => 'rcps-widget-img lazyload',
+					'alt'   => get_the_title(),
+				)
+			);
 			?>
 		</a>
 	</div><!-- .rcps-widget-image -->
 
-	<a href="<?php echo esc_url( mytheme_get_recipe_url() ); ?>" <?php echo ( mytheme_is_external_recipe() && mytheme_get_option( 'link_external_recipes_to_source' ) ? 'target="_blank" rel="noopener"' : '' ); ?> class="rcps-widget-recipes-title"><?php the_title(); ?></a>
+	<a href="<?php echo esc_url(mytheme_get_recipe_url()); ?>" <?php echo (mytheme_is_external_recipe() && mytheme_get_option('link_external_recipes_to_source') ? 'target="_blank" rel="noopener"' : ''); ?> class="rcps-widget-recipes-title"><?php the_title(); ?></a>
 
 	<ul class="rcps-meta rcps-meta-widget">
-		<?php if ( 'recipe' === get_post_type() ) : ?>
-			<?php if ( mytheme_get_option( 'enable_ratings' ) ) : ?>
+		<?php if ('recipe' === get_post_type()) : ?>
+			<?php if (mytheme_get_option('enable_ratings')) : ?>
 				<?php
-				$votes_percent = get_post_meta( get_the_ID(), 'custom_meta_votes_percent', true );
-				if ( '0' !== $votes_percent ) :
-					$meta_percent = absint( round( $votes_percent, 0 ) ) . '%';
+				$votes_percent = get_post_meta(get_the_ID(), 'custom_meta_votes_percent', true);
+				if ('0' !== $votes_percent) :
+					$meta_percent = absint(round($votes_percent, 0)) . '%';
 				else :
-					$meta_percent = esc_html__( 'No Votes', 'recipes' );
+					$meta_percent = esc_html__('No Votes', 'recipes');
 				endif;
 				?>
 
 				<li class="meta-percent">
-					<svg class="rcps-icon rcps-icon-meta"><use xlink:href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons.svg#icon-thumbs-up"/></svg><?php echo esc_html( $meta_percent ); ?>
+					<svg class="rcps-icon rcps-icon-meta">
+						<use xlink:href="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons.svg#icon-thumbs-up" />
+					</svg><?php echo esc_html($meta_percent); ?>
 				</li>
 			<?php endif; ?>
 
-			<?php $total_time = get_post_meta( get_the_ID(), 'custom_meta_total_time', true ); ?>
-			<?php if ( is_numeric( $total_time ) && 0 < $total_time ) : ?>
+			<?php if (get_post_meta(get_the_ID(), 'custom_meta_external_url', true) && get_post_meta(get_the_ID(), 'custom_meta_external_site', true)) : ?>
 				<li><svg class="rcps-icon rcps-icon-meta">
-					<use xlink:href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons.svg#icon-clock"/></svg><?php mytheme_minutes_to_hr_min( $total_time ); ?>
-				</li>
-			<?php endif; ?>
-
-			<?php if ( get_post_meta( get_the_ID(), 'custom_meta_external_url', true ) && get_post_meta( get_the_ID(), 'custom_meta_external_site', true ) ) : ?>
-				<li><svg class="rcps-icon rcps-icon-meta">
-					<use xlink:href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons.svg#icon-external-link-square"/></svg>
-					<a href="<?php echo esc_url( get_post_meta( get_the_ID(), 'custom_meta_external_url', true ) ); ?>" target="_blank" rel="noopener"><?php echo esc_html( get_post_meta( get_the_ID(), 'custom_meta_external_site', true ) ); ?></a>
+						<use xlink:href="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons.svg#icon-external-link-square" />
+					</svg>
+					<a href="<?php echo esc_url(get_post_meta(get_the_ID(), 'custom_meta_external_url', true)); ?>" target="_blank" rel="noopener"><?php echo esc_html(get_post_meta(get_the_ID(), 'custom_meta_external_site', true)); ?></a>
 				</li>
 
-				<?php if ( mytheme_get_option( 'link_external_recipes_to_source' ) ) : ?>
+				<?php if (mytheme_get_option('link_external_recipes_to_source')) : ?>
 					<li>
-						<a href="<?php echo esc_url( get_permalink() ); ?>" title="<?php the_title(); ?>"><svg class="rcps-icon rcps-icon-meta"><use xlink:href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons.svg#icon-link"/></svg><span class="screen-reader-text"><?php esc_html_e( 'Permalink', 'recipes' ); ?></span></a>
+						<a href="<?php echo esc_url(get_permalink()); ?>" title="<?php the_title(); ?>"><svg class="rcps-icon rcps-icon-meta">
+								<use xlink:href="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons.svg#icon-link" />
+							</svg><span class="screen-reader-text"><?php esc_html_e('Permalink', 'recipes'); ?></span></a>
 					</li>
 				<?php endif; ?>
 			<?php endif; ?>
 
-		<?php elseif ( 'post' === get_post_type() ) : ?>
+		<?php elseif ('post' === get_post_type()) : ?>
 
 			<li>
-				<svg class="rcps-icon rcps-icon-meta"><use xlink:href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons.svg#icon-date"/></svg><?php echo get_the_date(); ?>
+				<svg class="rcps-icon rcps-icon-meta">
+					<use xlink:href="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons.svg#icon-date" />
+				</svg><?php echo get_the_date(); ?>
 			</li>
 		<?php endif; ?>
 	</ul>
